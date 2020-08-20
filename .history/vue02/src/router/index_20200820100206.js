dd65c4a72c2df90a97e04ed4c27291d8367876f5
渣渣 <resab@qq.com>
2020-08-20 11:05:30 +0800

    loing .vue

--- .history/vue02/src/router/index_20200820100206.js
+++ .history/vue02/src/router/index_20200820100206.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/views/HelloWorld'
-import login from '@/views/login'
 
 Vue.use(Router)
 
@@ -11,10 +10,6 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    } ,{
-      path: '/login',
-      name: 'login',
-      component: login
     }
   ]
 })
